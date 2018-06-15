a88a1aea3fbf1a7b9c8e9836bb801da17129ca92
BaoYong-1 <534138762@qq.com>
2018-06-15 13:43:50 +0800

    2018-06-15 代码测试通过

---
+++ others/Data_Test.py
@@ -0,0 +1,47 @@
+import xlrd
+import xlwt
+import os
+
+l_p = []  # 定义两个全局list，分别存储原始和目的需要对比的数据
+l_t = []
+
+
+def read_excel():
+    scrpath = 'F:\\PyTesting\\AutoTset\\log\\'
+    wb_pri = xlrd.open_workbook(scrpath + 'chart1.xls')  # 打开原始文件
+    wb_tar = xlrd.open_workbook(scrpath + 'GPS_TARG.xlsx')  # 打开目标文件
+    wb_result = xlwt.Workbook()  # 新建一个文件，用来保存结果
+    sheet_result = wb_result.add_sheet('result', cell_overwrite_ok=True)
+    result_i = 0
+    result_j = 0
+    sheet_pri = wb_pri.sheet_by_index(0)
+    sheet_tar = wb_tar.sheet_by_index(0)
+    print(sheet_pri.name, sheet_tar.name)
+    # 为什么是取这一列，因为这就是需要对比的数据阿
+    l_p = sheet_pri.col_values(1)
+    l_t = sheet_tar.col_values(1)
+
+    # 求参数在pri（原始数据）中存在，而在tar（目标）中不存在的
+    tmp_pd = list(set(l_p).difference(set(l_t)))
+    # 求参数在tar中存在，而在pri中不存在的
+    tmp_td = list(set(l_t).difference(set(l_p)))
+
+    if result_i < result_j:
+        result_i = result_j
+    else:
+        result_j = result_i
+    for pd_i in tmp_pd:
+        result_i = result_i + 1
+        sheet_result.write(result_i, 0, sheet_pri.name)
+        sheet_result.write(result_i, 2, pd_i)
+    for td_i in tmp_td:
+        result_j = result_j + 1
+        sheet_result.write(result_j, 1, sheet_tar.name)
+        sheet_result.write(result_j, 3, td_i)
+
+    # 好了，可以去名为result的excel中查看结果了
+    wb_result.save(scrpath + 'result.xls')
+
+
+if __name__ == '__main__':
+    read_excel()
